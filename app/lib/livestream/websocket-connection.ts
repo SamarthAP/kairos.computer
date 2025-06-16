2327966d0985e631a6b231cb43a23a4a46bcfa44
Samarth Patel <samarth.patel.22@hotmail.com>
2025-06-15 18:40:55 -0700

    fix api

--- app/lib/livestream/websocket-connection.ts
+++ app/lib/livestream/websocket-connection.ts
@@ -1,6 +1,6 @@
 const WEBSOCKET_URL =
   process.env.NODE_ENV === "production"
-    ? "wss://api.kairos.computer/core/v1"
+    ? "wss://api-prod.kairos.computer/core/v1"
     : "wss://api-dev.kairos.computer/core/v1";
 
 export const getCreateWorkflowWebSocketUrl = () => {
